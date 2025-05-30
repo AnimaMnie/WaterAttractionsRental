3e3000fe6f3ecb774ba47de5cc331f15f177b0ec
Szymon <szymon2206033@gmail.com>
2025-05-30 21:05:27 +0200

    Create Project WaterAttractionsRental

---
+++ WaterAttractionsRental/src/test/java/org/example/waterattractionsrental/WaterAttractionsRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.waterattractionsrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterAttractionsRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
